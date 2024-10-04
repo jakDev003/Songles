578ab028f9a4c3d3a13a68fe04274f22516cd20f
Josh K <linkspace003@fastmail.com>
2024-10-04 10:25:12 -0400

    added hour value when importing time from csv

--- songles/Data/Utilities.cs
+++ songles/Data/Utilities.cs
@@ -21,13 +21,28 @@ namespace songles.Data
                 var records = csv.GetRecords<CsvSong>();
                 foreach (var record in records)
                 {
+                    /// adjust the time to the correct format
+                    /// by adding the value for hours to by used
+                    /// in the TimeOnly constructor
+                    TimeOnly timeToAdd;
+                    if (record.Time != null)
+                    {
+                        var time = record.Time.Value.ToString().Split(':');
+                        timeToAdd = new TimeOnly(0, int.Parse(time[0]), int.Parse(time[1].Split(' ')[0]));
+                    }
+                    else
+                    {
+                        timeToAdd = new TimeOnly(0, 0, 0);
+                    }
+
+                    /// add the song to the database
                     var songToAdd = new Song
                     {
                         TrackName = record.TrackName,
                         Artist = record.Artist,
                         Album = record.Album,
                         Genre = record.Genre,
-                        Time = record.Time
+                        Time = timeToAdd
                     };
                     await db.Songs.AddAsync(songToAdd);
                 }
